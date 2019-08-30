b885eeae4672bf9d60a26b14959a29179bb2c51d
igor aleschenkov <igor.aleschenkov@gmail.com>
2019-08-30 15:24:12 +0300

    Empty template

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"accuweather adapter settings": {
+		"en": "Adapter settings for accuweather",
+		"de": "Adaptereinstellungen für accuweather",
+		"ru": "Настройки адаптера для accuweather",
+		"pt": "Configurações do adaptador para accuweather",
+		"nl": "Adapterinstellingen voor accuweather",
+		"fr": "Paramètres d'adaptateur pour accuweather",
+		"it": "Impostazioni dell'adattatore per accuweather",
+		"es": "Ajustes del adaptador para accuweather",
+		"pl": "Ustawienia adaptera dla accuweather",
+		"zh-cn": "accuweather的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "option2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
